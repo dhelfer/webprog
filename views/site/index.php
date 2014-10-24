cd1070dcbe4465247c4eace04ed107c430ed14f1
ijoah <schlaepfer.p@sunrise.ch>
2014-10-24 18:24:25 +0200

    view

--- views/site/index.php
+++ views/site/index.php
@@ -9,15 +9,16 @@ $this->title = 'SolCity';
         <div class="row">
         
         <?php
-            $articles = \app\models\Article::find()->all();
+            $articles = \app\models\Article::find()->where(['released' => '1'])->all();
             $items = array();
-            $counter = 0;
+            $counter=0;
+            if(count($articles) > 0):
             foreach ($articles as $article):
         ?>   
-            <div class=" col-lg-4" id="featArticle">
+            <div class=" col-lg-6" id="featArticle">
                 <div class="panel panel-default inArticle">
                     <div class="panel-heading inArticleHead" >
-                        <h2><?php echo $article->title; ?></h2>
+                        <h3><?php echo $article->title; ?></h3>
                     </div>
                     
                     <div class="panel-body panel-body-extra">
@@ -29,17 +30,17 @@ $this->title = 'SolCity';
                         <p class="readMore"><a class="btn btn-default" href="<?php echo $_SERVER['PHP_SELF'] ?>?r=article/view&id=<?php echo $article->articleId ?>">Weiterlesen</a></p>
                     <?php endif; ?>
                      </div>
-                     
                 </div>
             </div>
             
             
         <?php
             $counter++;
-            if ($counter%3==0){
+            if($counter%2==0){
                 echo "</div><div class='row'>";
             }
             endforeach;
+            endif;
         ?>
         
         </div>
